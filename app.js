f41e32ed73e6a545252df2cb2cabac059b414c36
Manuel Garcia F <mgarciafrigo@gmail.com>
2020-09-21 13:09:53 -0300

    move to express

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
 
-function renderhtml(filepath, res) {
-  fs.readFile(filepath, (err, data) => {
-    if (err) {
-      res.writeHead(504, { 'Content-Type': 'text/html' });
-      return res.end("<h1>504 You shouldn't be seeing this</h1>");
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end();
-  });
-}
+const app = express();
 
-http
-  .createServer((req, res) => {
-    const query = url.parse(req.url);
+app.use(express.static(path.join(__dirname, 'public')));
 
-    switch (query.pathname) {
-      case '/':
-        renderhtml('./index.html', res);
-        break;
-      case '/about':
-        renderhtml('./about.html', res);
-        break;
-      case '/contact-me':
-        renderhtml('./contact-me.html', res);
-        break;
-      default:
-        renderhtml('./404.html', res);
-    }
-  })
-  .listen(8080);
+app.get('/', (_, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/about', (_, res) => {
+  res.sendFile(path.join(__dirname, '/public/about.html'));
+});
+
+app.get('/contact-me', (_, res) => {
+  res.sendFile(path.join(__dirname, '/public/contact-me.html'));
+});
+
+// Handle 404 errors
+app.use((_, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/public/404.html'));
+});
+
+app.listen(8080);
+console.log('Starting server on http://localhost:8080');
